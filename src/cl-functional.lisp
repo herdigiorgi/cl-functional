0f539496d9bc36cdf00d3a02675b3cf43171464b
Hernan Ezequiel Di Giorgi <hernan.digiorgi@gmail.com>
2015-07-22 15:58:12 -0300

    Add missing data-structures exports

--- src/cl-functional.lisp
+++ src/cl-functional.lisp
@@ -1,4 +1,5 @@
 (in-package :cl-user)
 (uiop:define-package :cl-functional
   (:use-reexport :cl-functional.tco
-                 :cl-functional.utils))
+                 :cl-functional.utils
+                 :cl-functional.data-structures))
